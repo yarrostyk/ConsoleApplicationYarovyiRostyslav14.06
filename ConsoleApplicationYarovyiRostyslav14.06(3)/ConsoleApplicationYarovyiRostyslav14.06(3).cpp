32c3159a8b9cf00c18463874bd3f06f88bca33f3
p45 <p45@209-4>
2025-06-14 14:44:53 +0300

    Numbers

--- ConsoleApplicationYarovyiRostyslav14.06(3)/ConsoleApplicationYarovyiRostyslav14.06(3).cpp
+++ ConsoleApplicationYarovyiRostyslav14.06(3)/ConsoleApplicationYarovyiRostyslav14.06(3).cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main()
 {
     cout << "We commit this code with VS studio!!!";
-    cout << "!!!";
     cout << "My name is Rostyk";
     cout << "I live in Lviv";
     cout << "Where is Roksolana?";
