3e1ca64d8b2285919d7f84049e1770d176f38bac
p45 <p45@209-4>
2025-06-14 14:36:02 +0300

    Text about history

--- ConsoleApplicationYarovyiRostyslav14.06(3)/ConsoleApplicationYarovyiRostyslav14.06(3).cpp
+++ ConsoleApplicationYarovyiRostyslav14.06(3)/ConsoleApplicationYarovyiRostyslav14.06(3).cpp
@@ -5,5 +5,7 @@ int main()
 {
     cout << "We commit this code with VS studio!!!";
     cout << "!!!";
-
+    cout << "My name is Rostyk";
+    cout << "I live in Lviv";
+    cout << "Where is Roksolana?";
 }
